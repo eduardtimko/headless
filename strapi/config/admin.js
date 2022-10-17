8bcd4ce8af58a4be5160010b5f140ab16ef79048
Anhelina Kavaleva-Yeusiuchenia <anhelina.ke@spur-i-t.com>
2022-10-17 16:36:53 +0300

    Update to Node v18

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '5f1fea5f9155c8636710a3b1e8ca0611'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'fe5VHj/Vh8b2RWEIF8rdNw==')
+  }
 });
